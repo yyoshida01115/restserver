a9b7b595cbfdfd9d49ec6771ee9f6898783f4b5e
yyoshida01115 <y.yoshida01115@gmail.com>
2019-05-25 22:44:53 +0900

    logger add

--- RestServerSample/client/getsample.py
+++ RestServerSample/client/getsample.py
@@ -6,7 +6,7 @@ Created on 2019/05/25
 import urllib.request
 
 if __name__ == '__main__':
-    req = urllib.request.Request('http://192.168.56.10:8888')
+    req = urllib.request.Request('http://192.168.56.10:18888')
     with urllib.request.urlopen(req) as res:
         body = res.read()
         print(body)
--- RestServerSample/server/main.py
+++ RestServerSample/server/main.py
@@ -6,14 +6,32 @@ Created on 2019/05/23
 from server import api_class
 import sys
 
+def server_logger():
+	from logging import getLogger,StreamHandler,FileHandler, DEBUG
+	logger = getLogger("RestServerLogger")
+	shandler = StreamHandler()
+	fhandler = FileHandler('../log/server.log')
+	shandler.setLevel(DEBUG)
+	fhandler.setLevel(DEBUG)
+	logger.setLevel(DEBUG)
+	logger.addHandler(shandler)
+	logger.addHandler(fhandler)
+	logger.propagate = False
+	return logger
+
 def server_main():
 	try:
+		logger=server_logger()
 		from wsgiref import simple_server
 		app = api_class.ApiServerFactory()
-		httpd = simple_server.make_server("0.0.0.0", 8888, app.getApiServer())
+		httpd = simple_server.make_server("0.0.0.0", 18888, app.getApiServer())
 		httpd.serve_forever()
 	except KeyboardInterrupt:
 		sys.exit(0)
+	except:
+		import traceback
+		logger.error(traceback.format_exc())
+		
 		
 if __name__ == '__main__':
 	server_main()
